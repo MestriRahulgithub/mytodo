ec7760f9fc148eca164ab38ba740fdfc0a4b932d
MestriRahulgithub <rahulmestrirahul@gmail.com>
2024-07-30 02:46:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import Footer from './Footer';
+import './App.css'; // Import App.css
+
+
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (todo.trim()) {
+      setTodos([...todos, { id: Date.now(), text: todo, completed: false }]);
+    }
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const renameTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        renameTodo={renameTodo}
+      />
+      <Footer />
     </div>
   );
 }
